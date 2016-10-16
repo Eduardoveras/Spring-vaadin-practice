f149a3bb00922e3e52107bbe48fc2f8e0fdf4f0a
Eduardo Veras <eduardo.storm@gmail.com>
2016-10-16 12:26:46 -0400

    empty project

---
+++ src/main/java/com/pucmm/UI/TodoUI.java
@@ -0,0 +1,20 @@
+package com.pucmm.UI;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by Eduardo veras on 16-Oct-16.
+ */
+
+@SpringUI
+@Theme("valo")
+public class TodoUI extends UI{
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+    }
+}
