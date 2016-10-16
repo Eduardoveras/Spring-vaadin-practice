54f684bf2b1ed2533e9d6fe8696f5b144b4e56a4
Eduardo Veras <eduardo.storm@gmail.com>
2016-10-16 13:12:32 -0400

    making it pretty, learinng

---
+++ src/main/java/com/pucmm/UI/TodoList.java
@@ -0,0 +1,14 @@
+package com.pucmm.UI;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.VerticalLayout;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Eduardo veras on 16-Oct-16.
+ */
+
+@SpringComponent @UIScope
+public class TodoList extends VerticalLayout {
+}
--- src/main/java/com/pucmm/UI/TodoUI.java
+++ src/main/java/com/pucmm/UI/TodoUI.java
@@ -3,18 +3,73 @@ package com.pucmm.UI;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Eduardo veras on 16-Oct-16.
  */
 
+
+
 @SpringUI
 @Theme("valo")
 public class TodoUI extends UI{
 
+    private VerticalLayout layout;
+
+    @Autowired
+    TodoList todoList;
+
     @Override
     protected void init(VaadinRequest request) {
+        setupLayout();
+        addHeader();
+        addForm();
+        addTodoList();
+        addButton();
 
+    }
+
+    public void setupLayout()
+    {
+        layout= new VerticalLayout();
+        layout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        setContent(layout);
+
+    }
+
+    public void addHeader()
+    {
+        Label header = new Label("TODO");
+        header.addStyleName(ValoTheme.LABEL_H1);
+        header.setSizeUndefined();
+        layout.addComponent(header);
+
+    }
+
+    public void addForm()
+    {
+        HorizontalLayout formLayout= new HorizontalLayout();
+        formLayout.setSpacing(true);
+        formLayout.setWidth("80%");
+        TextField taskField = new TextField();
+        Button addButton = new Button("ADD");
+        formLayout.addComponents(taskField,addButton);
+        layout.addComponent(formLayout);
+
+    }
+
+    public void addTodoList()
+    {
+        todoList.setWidth("80%");
+        layout.addComponent(todoList);
+    }
+
+    public void addButton()
+    {
+        Button delete = new Button("DELETE");
+        layout.addComponent(delete);
     }
 }
