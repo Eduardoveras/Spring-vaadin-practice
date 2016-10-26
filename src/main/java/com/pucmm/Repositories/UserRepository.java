03744f7850940977a50a2f66370195868f91da0e
Djsiclait <djidjellypas92@gmail.com>
2016-10-26 08:07:30 -0400

    Create UserRepository

---
+++ src/main/java/com/pucmm/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Djidjelly Siclait on 10/26/2016.
+ */
+package com.pucmm.Repositories;
+
+import com.pucmm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    @Query("select u from User u where u.email = :email and u.password = :pass")
+    User findByEmailAndPassword(@Param("email") String email, @Param("pass") String password);
+}
