f12bffc031da41f438e9439d3b767cd56e1f04d6
Djsiclait <djidjellypas92@gmail.com>
2016-10-26 10:29:50 -0400

    Add initial layout to UserView

--- src/main/java/com/pucmm/UI/UserView.java
+++ src/main/java/com/pucmm/UI/UserView.java
@@ -5,10 +5,13 @@ package com.pucmm.UI;
 
 import com.pucmm.Services.AccessControlService;
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ForkJoinPoolFactoryBean;
 
 @SpringUI(path = "/userInfo")
 @Theme("valo")
@@ -17,8 +20,38 @@ public class UserView extends UI {
     @Autowired
     private AccessControlService accessControlService;
 
+    private HorizontalLayout layout;
+    private FormLayout editPassword;
+    private VerticalLayout infoLayout;
+
     @Override
     public void init(VaadinRequest vaadinRequest){
 
+        if (accessControlService.fetchAllRegisteredUser().isEmpty())
+            getUI().getPage().setLocation("/");
+
+        setupLayout();
+        addHeader();
     }
+
+    public void setupLayout()
+    {
+        Page.getCurrent().setTitle("Login");
+
+        layout = new HorizontalLayout();
+        layout.setSpacing(true);
+        layout.setMargin(true);
+        layout.setSizeFull();
+        layout.setDefaultComponentAlignment(Alignment.TOP_CENTER);
+        setContent(layout);
+    }
+
+    public void addHeader()
+    {
+        Label header = new Label("YOUR ARE VIEWING THE PROFILE ASSIGNED TO " + accessControlService.fetchAllRegisteredUser().get(0).getFullName());
+        header.addStyleName(ValoTheme.LABEL_H3);
+        header.setSizeUndefined();
+        layout.addComponent(header);
+    }
+
 }
